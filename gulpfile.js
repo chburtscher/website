186b891c43ab71a5b09e981523a53ea8add49000
Christina Burtscher <christina.burtscher@gmx.at>
2017-01-14 17:10:22 +0100

    Added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,32 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyHtml = require("gulp-minify-html");
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
     return gulp.src('./src/sass/style.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', ['sass', 'html'], function () {
+gulp.task('watch', ['sass', 'html', 'browser-sync'], function () {
     gulp.watch(['./src/**/*.scss'], ['sass']);
     gulp.watch(['./src/*.html'], ['html']);
+
 });
 
 gulp.task('html', function () {
     gulp.src('./src/*.html') // path to your files
         .pipe(minifyHtml())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
 });
